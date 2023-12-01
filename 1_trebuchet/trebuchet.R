e6918cecd0e02965e9310636ffdd6f66345ab034
Emily Baker <emilyautumnbaker@gmail.com>
2023-12-01 15:46:26 -0500

    update

--- 1_trebuchet/trebuchet.R
+++ 1_trebuchet/trebuchet.R
@@ -3,12 +3,8 @@ library(tidyverse)
 input_data = read.delim("1_trebuchet/input.txt", header = FALSE)
 
 for (line in input_data) {
-    nums <- str_extract_all(line, "\\d")
-    print(nums)
-    print(nums[3])
-    # first = nums[1]
-    # last = nums[dim(nums)]
-    
-    # print(first)
-    # print(last)
+    nums <- c(str_extract_all(line, "\\d"))
 }
+
+print(nums[1])
+print(nums[1000])
